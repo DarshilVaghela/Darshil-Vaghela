ef17d8970db8dd71bc5ad0b04f70706d820c0686
Darshil Vaghela <117272682+DarshilVaghela@users.noreply.github.com>
2024-06-11 06:41:27 +0530

    main.js

---
+++ main.js
@@ -0,0 +1,94 @@
+document.addEventListener("contextmenu", function(event){
+event.preventDefault();
+alert('Right Click is Disabled!!!');    
+}, false);
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    delay: 200,
+    
+//     reset: true
+});
+
+sr.reveal(' .work__img, .contact__input',{interval: 200}); 
+ScrollReveal({ reset: true });
+
+ScrollReveal().reveal(".show-once", {
+  reset: false
+});
+
+ScrollReveal().reveal(' .skills__data, .home__data, .about__img, .skills__subtitle, .skills__text', {
+  duration: 3000,
+  origin: "top",
+  distance: "400px",
+  easing: "cubic-bezier(0.5, 0, 0, 1)",
+  rotate: {
+    x: 20,
+    z: -10
+  }
+});
+
+
+ScrollReveal().reveal('.footer__social', {
+  duration: 4000,
+  scale: 0.85
+});
+
+ScrollReveal().reveal('.home__img, .about__subtitle, .about__text, .skills__img', {
+  delay: 500,
+  duration: 2000,
+});
+
+ScrollReveal().reveal(".slide-up", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "100px",
+  easing: "cubic-bezier(.37,.01,.74,1)",
+  opacity: 0.3,
+  scale: 0.5
+});
